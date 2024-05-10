55e7e86f9d389a408d14d754535990ff5c584c32
Sibbe Bakker <sibbe.l.bakker@gmail.com>
2024-05-10 11:23:31 +0200

    add gcc; remove colabfold

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 echo "Pixi is running the installer script!"
 
-echo "Linking the gpu to colabfold"
-
-pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-
 echo "compiling the assembler"
 cd CombinatorialAssembler
 make
